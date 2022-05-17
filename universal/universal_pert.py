78cfadd74d4238d1e017f3cab4f4168789bbc6bc
riiswa <wradji64@gmail.com>
2022-05-17 22:55:47 +0200

    Try to add gpu support

--- universal/universal_pert.py
+++ universal/universal_pert.py
@@ -79,6 +79,7 @@ def universal_perturbation(dataset, f, delta=0.2, max_iter_uni=np.inf, xi=10, p=
 
                     # Project on l_p ball
                     v = proj_lp(v, xi, p)
+            print(np.linalg.norm(v))
 
         itr = itr + 1
 
