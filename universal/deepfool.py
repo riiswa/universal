77684542faf5b13870ac6fbbeb7e68632763772a
riiswa <wradji64@gmail.com>
2022-05-17 23:42:21 +0200

    Try to add gpu support

--- universal/deepfool.py
+++ universal/deepfool.py
@@ -51,7 +51,7 @@ def deepfool(image, net, num_classes=10, overshoot=0.02, max_iter=10, device='cp
         # compute r_i and r_tot
         # Added 1e-4 for numerical stability
         r_i = ((pert + 1e-4) * w / torch.linalg.norm(w)).to(device)
-        r_tot = np.float32((r_tot + r_i).cpu())
+        r_tot = r_tot + r_i
 
         pert_image = image.to(device) + (1 + overshoot) * (r_tot.to(device))
 
