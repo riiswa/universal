9524984b4bfbbc2e9f4b8eddae9e823fd40b4419
riiswa <wradji64@gmail.com>
2022-05-18 04:24:52 +0200

    Try to add gpu support

--- universal/deepfool.py
+++ universal/deepfool.py
@@ -39,12 +39,13 @@ def deepfool(image, net, num_classes=10, overshoot=0.02, max_iter=10, device='cp
 
             # set new w_k and new f_k
             w_k = cur_grad - grad_orig
+            print(torch.linalg.norm(fs[0, I[k]]), torch.linalg.norm(fs[0, I[0]]))
             f_k = (fs[0, I[k]] - fs[0, I[0]]).data
-            print("w_k", torch.linalg.norm(w_k))
-            print("f_k", torch.linalg.norm(f_k))
+            #print("w_k", torch.linalg.norm(w_k))
+            #print("f_k", torch.linalg.norm(f_k))
 
             pert_k = f_k.abs() / torch.linalg.norm(w_k.flatten())
-            print("pert_k", loop_i, torch.linalg.norm(pert_k))
+            #print("pert_k", loop_i, torch.linalg.norm(pert_k))
 
             # determine which w_k to use
             if pert_k < pert:
