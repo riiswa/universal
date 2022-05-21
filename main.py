c9b57cd484b36003d497fc7c56ad1c95ea4ae406
riiswa <wradji64@gmail.com>
2022-05-21 08:35:13 +0200

    Generalization of the experiment

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import os
 import gc
 
-import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
-from PIL import Image
 
-from universal.data_loading import download_training_data, download_testing_data, get_trainval_dataset, \
-    get_test_dataset, VOCDataset, test_transforms, train_transforms, classes, universal_transforms, normalize
-from universal.deepfool import deepfool
-from universal.plot import plot_images
+from universal.data_loading import download_training_data, get_trainval_dataset, VOCDataset, test_transforms, \
+    train_transforms, classes, universal_transforms, normalize
+from universal.experiment1 import experiment1
 from universal.universal_pert import universal_perturbation
 from universal.vgg11_model import *
 import logging
 import torch
-import torch.nn as nn
-from torch.utils.data import RandomSampler, DataLoader, Subset
 import argparse
-import torchvision.models as models
-import random
 
 if __name__ == '__main__':
     logging.basicConfig(
@@ -32,10 +25,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute universal perturbation on VGG-11.')
     parser.add_argument('-s', '--state-dict', type=str, help='File path of a pretrained state dict', default=None)
     parser.add_argument('--train-data', type=str, help='Directory that contains train data', default='.data/VOC2012')
-    parser.add_argument('--test-data', type=str, help='Directory that contains test data', default='.data/VOC2012_TEST')
     parser.add_argument('-f', '--force-download', action='store_true', default=False)
     parser.add_argument('-d', '--device', type=str, help='', default='cuda' if torch.cuda.is_available() else 'cpu')
-    #parser.add_argument('-v', '--visualize', help='', action='store_true', default=False)
     parser.add_argument('-p', '--perturbation', type=str, help='', default=None)
     parser.add_argument('-o', '--output', type=str, help='', default=None)
     parser.add_argument('-b', '--batch-size', type=int, help='', default=25)
@@ -54,28 +45,19 @@ if __name__ == '__main__':
     logging.info(f"Prepare data. Train/validation data: {args.train_data}, Test data: {args.test_data}")
     os.makedirs('.data/', exist_ok=True)
     download_training_data(args.train_data, args.force_download)
-    # download_testing_data(args.test_data, args.force_download)
     train_data_df, valid_data_df = get_trainval_dataset(args.train_data)
-    # test_data_df = get_test_dataset(args.test_data)
 
     train_data = VOCDataset(train_data_df, args.train_data, train_transforms)
     valid_data = VOCDataset(valid_data_df, args.train_data, test_transforms)
-    # test_data = VOCDataset(test_data_df, args.test_data, test_transforms)
 
     if args.state_dict:
         logging.info(f"State dict {args.state_dict} found. Load pretrained weights.")
         model.load_state_dict(torch.load(args.state_dict))
         model.eval()
     else:
-        logging.info(f"State dict not found.")
-        pretrained_model = models.vgg11_bn(pretrained=True)
-
-        IN_FEATURES = pretrained_model.classifier[-1].in_features
-
-        final_fc = nn.Linear(IN_FEATURES, OUTPUT_DIM)
-        pretrained_model.classifier[-1] = final_fc
-        model.load_state_dict(pretrained_model.state_dict())
-        # TODO
+        logging.info("State dict not found (--s). "
+                     "Please see the universal-voc-dataset.ipynb notebook to train the algorithm.")
+        exit()
     del train_data
 
     model.to(device)
@@ -123,7 +105,6 @@ if __name__ == '__main__':
             xi=2000/255,
             p=2,
             max_iter_uni=args.max_iter,
-            #input_vector=np.load(args.input) if args.input else None,
             device=device
         )
 
@@ -131,61 +112,15 @@ if __name__ == '__main__':
         np.save(args.output, v)
 
     if args.exp1:
-        with torch.no_grad():
-            norms = np.linspace(0., 10000, 50)
-
-            random_v = np.random.rand(1, 3, 224, 224)
-            random_v = random_v.astype(np.float32)
-            v_norm = np.linalg.norm(v[0])
-            random_v_norm = np.linalg.norm(random_v)
-
-            dataset = torch.stack([valid_data[i][0] for i in range(len(valid_data))])
-            num_images = len(valid_data)
-
-            first_time = True
-            est_labels_orig = np.zeros((num_images))
-
-            f1 = []
-            f2 = []
-
-            num_batches = np.int(np.ceil(np.float(num_images) / np.float(args.batch_size)))
-
-            for norm in tqdm(norms):
-                norm = norm / 255
-                normalized_v = v * (norm / v_norm)
-                normalized_random_v = random_v * (norm / random_v_norm)
-                # print(norm, np.linalg.norm(np.abs(normalized_v)), np.linalg.norm(np.abs(normalized_random_v)))
-
-                # Perturb the dataset with computed perturbation
-                dataset_perturbed = dataset + torch.from_numpy(normalized_v[0])
-                dataset_perturbed2 = dataset + torch.from_numpy(normalized_random_v)
-
-                est_labels_pert = np.zeros((num_images))
-                est_labels_pert2 = np.zeros((num_images))
-
-                # Compute the estimated labels in batches
-                for ii in range(0, num_batches):
-                    if device == 'cuda':
-                        torch.cuda.empty_cache()
-                    m = (ii * args.batch_size)
-                    M = min((ii + 1) * args.batch_size, num_images)
-                    if first_time:
-                        est_labels_orig[m:M] = np.argmax(classifier(dataset[m:M, :, :, :].to(device)).detach().cpu().numpy(), axis=1).flatten()
-
-                    est_labels_pert[m:M] = np.argmax(classifier(dataset_perturbed[m:M, :, :, :].to(device)).detach().cpu().numpy(), axis=1).flatten()
-                    est_labels_pert2[m:M] = np.argmax(classifier(dataset_perturbed2[m:M, :, :, :].to(device)).detach().cpu().numpy(), axis=1).flatten()
-                first_time = False
-                fooling_rate = float(np.sum(est_labels_pert != est_labels_orig) / float(num_images))
-                fooling_rate2 = float(np.sum(est_labels_pert2 != est_labels_orig) / float(num_images))
-                print(fooling_rate)
-                print(fooling_rate2)
-                f1.append(fooling_rate)
-                f2.append(fooling_rate2)
-
-            print(f1)
-            print(f2)
-
-            np.save("exp1.npy", np.array([f1, f2]))
-
-
-
+        experiment1(
+            np.linspace(0., 10000, 10), [v[0],
+                                         np.random.uniform(
+                                             low=v.min(),
+                                             high=v.max(),
+                                             size=(1, 3, 224, 224)
+                                         ).astype(np.float32)],
+            torch.stack([valid_data[i][0] for i in range(len(valid_data))]),
+            classifier,
+            args.bath_size,
+            device
+        )
---
+++ universal/experiment1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+def experiment1(norms, perturbations, dataset, f, batch_size, device):
+    with torch.no_grad():
+        perturbations_norm = [np.linalg.norm(p) for p in perturbations]
+        num_images = len(dataset)
+        num_perts = len(perturbations)
+        first_time = True
+        est_labels_orig = np.zeros(num_images)
+
+        fooling_rates = [[]] * len(perturbations)
+
+        num_batches = np.int(np.ceil(np.float(num_images) / np.float(batch_size)))
+
+        for norm in (pbar := tqdm(norms)):
+            norm = norm / 255
+            normalized_perturbations = [perturbations[i] * (norm / perturbations_norm[i]) for i in range(num_perts)]
+
+            perturbed_datasets = [dataset + torch.from_numpy(normalized_perturbations[i]) for i in range(num_perts)]
+
+            est_labels_perts = [np.zeros(num_images) for _ in range(num_perts)]
+
+            # Compute the estimated labels in batches
+            for ii in range(0, num_batches):
+                if device == 'cuda':
+                    torch.cuda.empty_cache()
+                m = (ii * batch_size)
+                M = min((ii + 1) * batch_size, num_images)
+                if first_time:
+                    est_labels_orig[m:M] = \
+                        np.argmax(f(dataset[m:M, :, :, :].to(device)).detach().cpu().numpy(), axis=1).flatten()
+                for i in range(num_perts):
+                    est_labels_perts[i][m:M] = \
+                        np.argmax(f(perturbed_datasets[i][m:M, :, :, :].to(device)).detach().cpu().numpy(), axis=1) \
+                            .flatten()
+                first_time = False
+                for i in range(num_perts):
+                    fooling_rates[i].append(float(np.sum(est_labels_perts[i] != est_labels_orig) / float(num_images)))
+                pbar.set_description([fr[-1] for fr in fooling_rates])
+        np.save("exp1.npy", np.array(fooling_rates))
+
